d488d0727be4209b77267f377c68bf904b8626ec
ganes <ganes@DESKTOP-G9FBDHJ>
2022-05-28 16:44:24 +0530

    commit123

--- src/pro123/Apple.java
+++ src/pro123/Apple.java
@@ -1,9 +1,23 @@
+//RbCA-20-114;
 package pro123;
 
 public class Apple {
+	String color;
+	Apple(String color)
+	{
+		this.color=color;
+	}
+	String getcolor()
+	{
+		return color;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Apple A1 = new Apple("Red");
+		String color = A1.getcolor();
+		System.out.println("color of apple is " + color);
+
 
 	}
 
